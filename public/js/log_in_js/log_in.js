55494e50c43b673519682319e748d91afdf2363f
HZ <hzeng1998@gmail.com>
2018-06-10 16:49:21 +0800

    修正了login模块部分bug

--- public/js/log_in_js/log_in.js
+++ public/js/log_in_js/log_in.js
@@ -1,86 +1,84 @@
-        var log_global = {
-            state:"",
-            security_username:"",
-        }
+var log_global = {
+    state:"",
+    security_username:"",
+}
 
-        var change_global = {
-            state:"",
-            msg:"",
-        }
+var change_global = {
+    state:"",
+    msg:"",
+};
 
-        function time() {
-            let time_div = document.getElementById('showtime');
-            let now = new Date();
-            time_div.innerHTML = now.getFullYear()+"年"+(now.getMonth()+1)+"月"+now.getDate()+"日"+now.getHours()+"时"+now.getMinutes()+"分"+now.getSeconds()+"秒";
-            setTimeout(time, 1000);
-        }
+function time() {
+    let time_div = document.getElementById('showtime');
+    let now = new Date();
+    time_div.innerHTML = now.getFullYear()+"年"+(now.getMonth()+1)+"月"+now.getDate()+"日"+now.getHours()+"时"+now.getMinutes()+"分"+now.getSeconds()+"秒";
+    setTimeout(time, 1000);
+}
 
-        function log() {
-            alert('请输入用户名和密码');
-            return;
-            var username = document.getElementById('username').value;
-            var password = document.getElementById('password').value;
-            if (username.length == 0 || password.length == 0) {
-                alert('请输入用户名和密码');
-                return;
+function log() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    if (username.length === 0 || password.length === 0) {
+        alert('请输入用户名和密码');
+    }
+    else {
+        window.location.href="/";
+/*
+        info = {'username':username, 'password':password}
+        text = JSON.stringify( info );
+
+        xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("POST","/account_user_login",true);
+        xmlhttp.setRequestHeader("Content-type","application/json");
+        xmlhttp.send(text);
+
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+                var temp = JSON.parse( xmlhtttp.responseText );
+                log_global.state = temp.site[0].state;
+                log_global.security_username = temp.site[1].security_username;
+                if (log_global.state === 'true') {
+                    window.location.href="index.html";
+                    return;
+                }
+                else
+                    alert('账号或密码错误！');
             }
-            else {
-                window.location.href="index.html";
-                return;
+        }
+        */
+    }
+    
+}
 
-                info = {'username':username, 'password':password}
-                text = JSON.stringify( info );
+function  change() {
+    alert('ppppppp');
+    var oldpswd = document.getElementById('oldpswd').value;
+    var newpswd1 = document.getElementById('newpswd1').value;
+    var newpswd2 = document.getElementById('newpswd2').value;
 
-                xmlhttp = new XMLHttpRequest();
-                xmlhttp.open("POST","/account_user_login",true);
-                xmlhttp.setRequestHeader("Content-type","application/json");
-                xmlhttp.send(text);
+    if (newpswd1 !== newpswd2)
+        alert('输入的新密码不一致！');
+    else {
+        info  = {'username':log_global.security_username, 'password':oldpswd, 'new_password':newpswd1};
+        text = JSON.stringify( info );
 
-                xmlhttp.onreadystatechange = function() {
-                    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                        var temp = JSON.parse( xmlhtttp.responseText );
-			            log_global.state = temp.site[0].state;
-			            log_global.security_username = temp.site[1].security_username;
-			            if (log_global.state == 'true') {
-			                window.location.href="index.html";
-                            return;
-			            }
-			            else
-			                alert('账号或密码错误！');
-                    }
+        xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("POST","/change_password",true);
+        xmlhttp.setRequestHeader("Content-type","application/json");
+        xmlhttp.send(text);
+
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+                var temp = JSON.parse( xmlhtttp.responseText );
+                change_global.state = temp.site[0].state;
+                change_global.msg = temp.site[1].msg;
+                if (change_global.state === 'true') {
+                    alert( change_global.msg );
                 }
             }
         }
-
-        function  change() {
-		    alert('ppppppp');
-            var oldpswd = document.getElementById('oldpswd').value;
-            var newpswd1 = document.getElementById('newpswd1').value;
-            var newpswd2 = document.getElementById('newpswd2').value;
-
-            if (newpswd1 != newpswd2)
-                alert('输入的新密码不一致！');
-            else {
-                info  = {'username':log_global.security_username, 'password':oldpswd, 'new_password':newpswd1};
-                text = JSON.stringify( info );
-
-                xmlhttp = new XMLHttpRequest();
-                xmlhttp.open("POST","/change_password",true);
-                xmlhttp.setRequestHeader("Content-type","application/json");
-                xmlhttp.send(text);
-
-                xmlhttp.onreadystatechange = function() {
-                    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                        var temp = JSON.parse( xmlhtttp.responseText );
-			            change_global.state = temp.site[0].state;
-			            change_global.msg = temp.site[1].msg;
-			            if (change_global.state == 'true') {
-			                alert( change_global.msg );
-			            }
-                    }
-                }
-            }
-		}
+    }
+}
 
 //        function setcookie( str1 ) {
 //			var temp = JSON.parse(str1);
